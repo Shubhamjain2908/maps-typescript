45403c15a19fbd249ff3d3038de16979838c3a94
Shubham Jain <Shubhamjain2908@gmail.com>
2020-06-12 21:08:54 +0530

    displaying the map

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
-import { Company } from './Company';
-import { User } from './User';
+// import { Company } from './Company';
+// import { User } from './User';
 
-const user = new User();
-console.log('CKMB', user);
+// const user = new User();
+// console.log('CKMB', user);
 
-const company = new Company();
-console.log('CKMB', company);
+// const company = new Company();
+// console.log('CKMB', company);
+
+new google.maps.Map(document.getElementById('map'), {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+});
