cc5351cebb8378bc329a946fbff3ef373a7adc91
Shubham Jain <Shubhamjain2908@gmail.com>
2020-06-12 21:54:30 +0530

    implementing interface

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import { Company } from './Company';
-// console.log('CKMB', company);
 import { CustomMap } from './CustomMap';
 import { User } from './User';
-// console.log('CKMB', user);
-const company = new Company();
 
 const user = new User();
+const company = new Company();
 const map = new CustomMap('map');
+
 map.addUserMarker(user);
 map.addUserMarker(company);
